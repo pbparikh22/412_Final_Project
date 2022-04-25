9aa6d83e0c39d885a56784ffb4c9ac64a58b47d9
nick <buchannf@dukes.jmu.edu>
2022-04-25 09:30:20 -0400

    Added README

--- exact_solution/exact_solution.py
+++ exact_solution/exact_solution.py
@@ -8,7 +8,19 @@
         
 
 def main():
-    number_of_nodes = int(input().split())
+    overveiw = [int(x) for x in input().split()]
+    number_of_nodes =  overveiw[0]
+    number_of_edges = overveiw[1]
+    graph = dict()
+    for i in range(number_of_nodes):
+        graph[i] = dict()
+
+    for index in range(number_of_edges):
+        temp = [int(x) for x in input().split()]
+        graph[temp[0]][temp[1]] = temp[2]
+
+    print(graph)
+
 
 if __name__ == "__main__":    
 
