8c328e3d3802b7bd6f96cd079d6e0ac3e89b3f80
nick <buchannf@dukes.jmu.edu>
2022-04-25 17:06:09 -0400

    Got Exact Solution working with my current test cases

--- exact_solution/exact_solution.py
+++ exact_solution/exact_solution.py
@@ -3,6 +3,53 @@
     Author: Nicholas Buchan
 """
 
+#Finds all the paths from s to t in the graph
+def findAllPaths(graph, s, t):
+    visited = [False] * len(graph)
+    simples = []
+
+    def pathFinder(graph, s, t, visited, path):
+        visited[s] = True
+        path.append(s)
+
+        if s == t:
+            simples.append(path.copy())
+        else:
+            for v in graph[s]:
+                if visited[v] == False:
+                    pathFinder(graph, v, t, visited, path)
+
+        path.pop()
+        visited[s] = False
+
+    pathFinder(graph, s, t, visited, [])
+
+    return simples
+
+#Loops through all the paths that were found in the graph
+#and calculates their respective length returning the longest ones
+def findLongestPath(graph, paths):
+    longestPathLength = 0
+    longestPath = None
+    
+    for path in paths:
+        currentPath = path
+        currentPathLength = 0
+        for node in range(len(path)):
+
+            if not node + 1 == len(path): 
+                u = path[node]
+                v = path[node + 1]
+                currentPathLength += graph[u][v]
+
+        if currentPathLength > longestPathLength:
+            longestPath = currentPath
+            longestPathLength = currentPathLength
+
+
+    return longestPath, longestPathLength
+
+
 
         
         
@@ -12,6 +59,9 @@ def main():
     number_of_nodes =  overveiw[0]
     number_of_edges = overveiw[1]
     graph = dict()
+    source = 0
+    sink = number_of_nodes - 1
+    
     for i in range(number_of_nodes):
         graph[i] = dict()
 
@@ -19,7 +69,11 @@ def main():
         temp = [int(x) for x in input().split()]
         graph[temp[0]][temp[1]] = temp[2]
 
-    print(graph)
+
+
+    solution = findLongestPath(graph, findAllPaths(graph, source, sink))
+
+    print("Longest path: {path}\nLongest Path Length: {length}".format(path = solution[0], length = solution[1]))
 
 
 if __name__ == "__main__":    
