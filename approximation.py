b1bb0ab38d565e60305183ec2f716f8db2e13a1d
Seth Walter <waltersb@dukes.jmu.edu>
2022-04-25 11:46:18 -0400

    starter file for approximation code

---
+++ approximation.py
@@ -0,0 +1,13 @@
+"""
+    names: Seth Walter
+
+    approximation solution for longest path np-complete
+"""
+def main():
+    pass
+
+def longest_path_approx():
+    pass
+
+if __name__ == "__main__":
+    main()
