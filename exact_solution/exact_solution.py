751130e96d1a8273adabd8cb841ec46fd5038d9d
nick <buchannf@dukes.jmu.edu>
2022-04-25 08:56:16 -0400

    Exact solution stubbed

---
+++ exact_solution/exact_solution.py
@@ -0,0 +1,15 @@
+"""   
+    NP Complete Project Exact Solution
+    Author: Nicholas Buchan
+"""
+
+
+        
+        
+
+def main():
+    number_of_nodes = int(input().split())
+
+if __name__ == "__main__":    
+
+    main()
