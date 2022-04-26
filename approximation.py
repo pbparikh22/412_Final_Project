a9de1f5cf42c055c2f3dfdad687f4acd8eb35779
Seth Walter <waltersb@dukes.jmu.edu>
2022-04-26 12:04:48 -0400

    dfs approximation seems to be working

--- approximation.py
+++ approximation.py
@@ -18,28 +18,54 @@ def main():
             graph[edge[1]] = []
         graph[edge[0]].append((edge[1],edge[2]))
     
+    for u in graph:
+        graph[u].sort(reverse=True, key=lambda v:v[1])
+    
     # change these values later to be read in from input
     s = 0
-    t = 5
+    t = 99
 
     # start our longest_path_approx from the source node
     # if finding a general longest path, start algorithm from each node
-    path, max_weight = longest_path_approx(graph, s, t)
+    path = longest_path_approx(graph, s, t)
 
     max_path = []
+    max_weight = 0
     v = s
     max_path.append(str(v))
-    while v != t:
-        v = path[v]
+    while v in path:
+        max_weight += path[v][1]
+        v = path[v][0]
         max_path.append(str(v))
     print(" => ".join(max_path))
-    print("Max weight of this path is", max_weight)
+    print("approximate max weight of this path is", max_weight)
     
+def longest_path_approx_DFS(graph, v, visited, path, t):
+    visited[v] = 0
+    if t in path:
+        return True
+    for k in path:
+        if t == path[k][0]:
+            return True
+    for u in graph[v]:
+        #if path != None: dont think I need this anymore
+        path[v] = (u[0], u[1])
+        isTrue = longest_path_approx_DFS(graph, u[0], visited, path, t)
+        if isTrue:
+            return True
+    return False
 
 def longest_path_approx(graph, s, t):
     # Need to make this dfs, its using bfs right now
     # NOTE the ending node is also broken, needs to be fixed to stop if it finds the end node
-    max_weight = 0
+    visited = {}
+    path = {}
+
+    longest_path_approx_DFS(graph, s, visited, path, t)
+    return path
+
+    # BFS approach, doesn't work as well as dfs in some cases
+    """max_weight = 0
     visited = {}
     path = {}
     stack = []
@@ -47,8 +73,8 @@ def longest_path_approx(graph, s, t):
     while len(stack) != 0:
         max_edge = (-1, -1)
         u = stack.pop()
-        if u == t:
-            break
+        #if u == t:
+            #break
         for v in graph[u]:
             if v[0] not in visited:
                 visited[u] = v[0]
@@ -58,20 +84,7 @@ def longest_path_approx(graph, s, t):
             max_weight += max_edge[1]
             path[u] = max_edge[0]
             stack.append(max_edge[0])
-    return path, max_weight
-
-    # we want to use dfs here to search for paths
-
-    # general idea is to find the heaviest edge weight, pick it and then
-    # continue to pick heaviest edge weights from there. Greedy approach
-
-    # other ideas include starting from second largest edge weight, third largest
-    # etc.
-
-    # we can also make this an anytime algorithm by allowing the greedy approach
-    # to keep searching and as time goes on hope it finds a better approximation
-    # NOTE probably wont do this just an idea
-    pass
+    return path, max_weight"""
 
 if __name__ == "__main__":
     main()
