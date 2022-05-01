1af8d547d20b84231087b22b9580934e440706be
Seth Walter <waltersb@dukes.jmu.edu>
2022-05-01 15:53:39 -0400

    finalized approximation tests and code

--- approximation_solution/run_test_cases.sh
+++ approximation_solution/run_test_cases.sh
@@ -7,9 +7,8 @@ echo "Test Case 1 Input:"
 echo
 cat ../exact_solution/test_cases/input1.txt
 echo "Test Case 1 Output:"
-time python3 approximation.py < ../exact_solution/test_cases/input1.txt > test_output.txt
+{ time python3 approximation.py < ../exact_solution/test_cases/input1.txt > test_output.txt; } 2>&1
 cat test_output.txt
-#cmp test_output.txt output1.txt || echo "TEST 1 FAILED"
 echo
 
 echo "Running Test Case 2"
@@ -18,9 +17,8 @@ echo
 cat ../exact_solution/test_cases/input2.txt
 echo
 echo "Test Case 2 Output:"
-time python3 approximation.py < ../exact_solution/test_cases/input2.txt > test_output.txt
+{ time python3 approximation.py < ../exact_solution/test_cases/input2.txt > test_output.txt; } 2>&1
 cat test_output.txt
-#cmp test_output.txt output2.txt || echo "TEST 2 FAILED"
 echo
 
 echo "Running Test Case 3"
@@ -29,15 +27,26 @@ echo
 cat ../exact_solution/test_cases/input3.txt
 echo
 echo "Test Case 3 Output:"
-time python3 approximation.py < ../exact_solution/test_cases/input3.txt > test_output.txt
+{ time python3 approximation.py < ../exact_solution/test_cases/input3.txt > test_output.txt; } 2>&1
 cat test_output.txt
-#cmp test_output.txt output3.txt || echo "TEST 3 FAILED"
 echo
-time python3 approximation.py < ../exact_solution/test_cases/input4.txt > test_output.txt
+
+echo "Running Test Case 4"
+echo "Test Case 4 Input:"
+cat ../exact_solution/test_cases/input5.txt
+echo
+echo "Test Case 4 Output:"
+{ time python3 approximation.py < ../exact_solution/test_cases/input5.txt > test_output.txt; } 2>&1
 cat test_output.txt
-time python3 approximation.py < ../exact_solution/test_cases/generatedInput.txt > test_output.txt
+echo
+
+echo "Running Test Case 5"
+echo "Test Case 5 Input:"
+cat ../exact_solution/test_cases/input4.txt
+echo
+echo "Test Case 5 Output:"
+{ time python3 approximation.py < ../exact_solution/test_cases/input4.txt > test_output.txt; } 2>&1
 cat test_output.txt
-time python3 approximation.py < ../exact_solution/test_cases/input5.txt > test_output.txt
-cat test_output.txt
+echo
 
 } > test_case_output.txt
